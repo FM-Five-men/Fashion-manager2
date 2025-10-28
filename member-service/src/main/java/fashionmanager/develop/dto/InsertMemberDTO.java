c1647f1dca4310d5ad5f5d1e3749b4a523aa5a9d
sangyoun <rabbit991020@naver.com>
2025-10-28 14:28:45 +0900

    회원가입 DB및 백엔드 로직 수정

--- member-service/src/main/java/fashionmanager/develop/dto/InsertMemberDTO.java
+++ member-service/src/main/java/fashionmanager/develop/dto/InsertMemberDTO.java
@@ -17,6 +17,11 @@ public class InsertMemberDTO {
     private char memberGender;
     private int memberHeight;
     private int memberWeight;
-    private String memberStatus = "ACTIVE";
+
+    // 새로 추가된 DB 컬럼(전화번호, 주소)
+    private String memberPhone;
+    private String memberAddress;
+
+    private String memberStatus = "활동중";
     private boolean memberMessageAllow = true;
 }
